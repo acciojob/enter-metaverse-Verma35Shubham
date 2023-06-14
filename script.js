5fa1faffe65c8c3cf7eb9d4771a66b60b0260951
Shubham Verma <verma.shubham2537@gmail.com>
2023-06-15 00:16:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let newString = "Entered Metaverse"
+
+function changeString() {
+	let para = document.getElementById("status")
+	para.innerText = `${newString}`
+	para.style.fontWeight = "bold"
+	para.style.fontSize = "28px"
+}
